921f633a5f0fa0be8745ac450f12d63956f45d0a
mj-mike-398745 <michael.baird@live.com>
2026-06-16 20:32:53 -0700

    Update config.h

--- keyboards/keebio/sinc/rev3/keymaps/mike/config.h
+++ keyboards/keebio/sinc/rev3/keymaps/mike/config.h
@@ -4,3 +4,5 @@
 #define RGB_MATRIX_DEFAULT_HUE 135   // teal — raise toward 170 for bluer, lower toward 110 for greener
 #define RGB_MATRIX_DEFAULT_SAT 255   // 255 = vivid, lower = softer/pastel
 #define RGB_MATRIX_DEFAULT_VAL 150   // brightness, 0–255 
+
+#define SPLIT_LAYER_STATE_ENABLE
